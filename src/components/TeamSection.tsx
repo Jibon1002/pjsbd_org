1547d6cbfab4085cfb5d91d644075a9fb695230d
Jibon Sir <150222594+Jibon1002@users.noreply.github.com>
2026-01-07 21:35:47 +0600

    Update TeamSection.tsx

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -31,7 +31,7 @@ const teamMembers = [
   {
     name: "মোঃ শরীফ হোসেন",
     role: "প্রচার সম্পাদক",
-    image: sharifImage,
+    image: sharipImage,
   },
    {
     name: "মোঃ শহিদুল ইসলাম শহিদ",
